2f92837f5c2d9b2cff8b0a0b715eea49f7cb0cd7
Javier Camacho <javier.camacho.dev@gmail.com>
2017-03-19 15:11:49 -0400

    Created basic errors package and linked it in.

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -26,9 +26,9 @@ Template.postSubmit.events({
 
         Meteor.call('postInsert', post, function (error, result) {
             // display the error to the user and abort      
-            if (error) return throwError(error.reason);
+            if (error) return Errors.throw(error.reason);
             if (result.postExists)
-                throwError('This link has already been posted');
+                Errors.throw('This link has already been posted');
             Router.go('postPage', { _id: result._id });
         });
     }
